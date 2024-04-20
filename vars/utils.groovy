2b8ceecfb399637b85db6fe4ba5f8ad922c4e4dd
Nishant Agarwal <ina000007@gmail.com>
2024-04-20 23:47:22 +0530

    Update utils.groovy

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,4 +1,5 @@
 def dockerImageBuild(containerName, tag){
+    sh "sudo su"
     sh "docker image prune -f"
     sh "docker build -t $containerName:$tag --pull --no-cache ."
     echo "Image build complete"
