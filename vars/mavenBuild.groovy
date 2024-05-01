1f2f55a0ffa2ecff83521414f80a276197aba17c
Nishant Agarwal <ina000007@gmail.com>
2024-05-01 23:15:23 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -3,41 +3,26 @@ def call(Map config = [:]){
     def productType = config.productType
     def pomfileName = config.pomfileName
     def containerName = config.containerName
-    def utils = new utils()
-    pipeline {
-        agent any
-        environment {
-            gitBranch = "${env.BRANCH_NAME}"
-            tag = "1.0.${BUILD_NUMBER}"
-        }
-        stages{
-            stage("Code Checkout"){
-                steps{
-                    git changelog: false, poll: false, url: gitURL, branch: gitBranch
-                }
-            }
-            stage("Code Build"){
-                steps{
-                    script {
-                        sh """
-                            ls -lart
-                            mvn clean install -f ${pomfileName}
-                        """
-                    }
-                }
-            }
-            stage("Docker Image Build"){
-                when {
-                    expression {
-                        productType == "docker"
-                    }
-                }
-                steps{
-                    script {
-                        utils.dockerImageBuild(containerName, tag)
-                    }
+
+ def podTemplates = new PodTemplates()
+
+  podTemplates.mavenTemplate {
+    node(POD_LABEL) {
+        stage('Get a Maven project') {
+            git gitURL
+            container('maven') {
+                stage('Build a Maven project') {
+                    sh "echo hello from $POD_CONTAINER" // displays 'hello from maven'
+                    sh 'mvn -B -ntp clean install'
                 }
             }
         }
-    }
+      // container('maven') {
+      //    stage('Build a Maven project') {
+      //        sh "echo hello from $POD_CONTAINER" // displays 'hello from maven'
+      //        sh 'mvn -B -ntp clean install'
+      //     }
+      // }
+     }
+  }
 }
