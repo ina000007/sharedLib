f913aa1970f18d91210b5435a8bc0d508ef9c8c4
Nishant Agarwal <ina000007@gmail.com>
2024-05-26 18:14:04 +0530

    Update utils.groovy

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -6,7 +6,18 @@ def dockerImageBuild(containerName, tag){
         sh "docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD"
         sh "docker tag $containerName:$tag $DOCKERHUB_USERNAME/$containerName:$tag"
         sh "docker push $DOCKERHUB_USERNAME/$containerName:$tag"
-}
-    
+  }  
     echo "Image build complete"
 }
+def kubernetesDeploy(){
+    withCredentials([file(credentialsId: 'SERVICEACCOUNT', variable: 'SERVICEACCOUNT')]) {
+        sh """
+            echo $SERVICEACCOUNT
+            gcloud auth activate-service-account --key-file=$SERVICEACCOUNT
+            gcloud auth list
+            gcloud config set project project-7-418106
+            gcloud container clusters get-credentials jenkins-cd --zone asia-south1-c --project project-7-418106
+            kubectl get node
+        """
+    } 
+} 
